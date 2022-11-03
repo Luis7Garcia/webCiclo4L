5464c88422c618659696bcbc6c5a3cd5e9950c3f
Luis17 <luis@gmail.com>
2022-11-02 23:55:40 -0500

    Add modules and components of airport user services, routes and flights.

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './assets/navbar/navbar.component';
+import { FooterComponent } from './assets/footer/footer.component';
+import { IndexComponent } from './assets/index/index.component';
+import { ErrorComponent } from './assets/error/error.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    IndexComponent,
+    ErrorComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
